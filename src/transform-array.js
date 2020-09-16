91d1f7d5e069b1ccafe296da48f707eb72d11942
Zedwag <zedwag@gmail.com>
2020-09-16 17:56:58 +0300

    feat: add solution for 6th task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error()
+  let newArr = []
+  let idxArr = new Array(arr.length)
+  idxArr.fill(0)
+  arr.forEach((el, idx) => {
+    if (el === '--discard-next') idx !== arr.length-1 ? idxArr[idx+1] = -5 : 1
+    else if (el === '--discard-prev') idx !== 0 ? idxArr[idx-1]-- : 1
+    else if (el === '--double-next') idx !== arr.length-1 ? idxArr[idx+1]++ : 1
+    else if (el === '--double-prev') idx !== 0 ? idxArr[idx-1]++ : 1
+    else idxArr[idx]++
+  })
+  idxArr.forEach((el, idx) => {
+    if (el > 0) for (let i = 0; i < el; i++) newArr.push(arr[idx])
+  })
+  return newArr
 };
