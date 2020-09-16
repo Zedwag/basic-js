2a33b46b73c6d07baebec7290413a97155b847c7
Zedwag <zedwag@gmail.com>
2020-09-16 12:16:26 +0300

    feat: add solution for 3rd task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false
+  let code = []
+  members.filter(e => typeof e === 'string').forEach(e => {
+    e = e.trim()
+    if (e.length) {
+      if ((e[0] >= 'A' && e[0] <= 'Z') || e[0] >= 'a' && e[0] <= 'z') code.push(e[0].toUpperCase())
+    }
+  })
+  return code.sort().join('')
 };
