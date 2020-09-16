6ebb8db3f6138cee7ec0038b567dbeb1273d2437
Zedwag <zedwag@gmail.com>
2020-09-16 21:40:45 +0300

    feat: add solution for 8th task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } = options
+  let output = ''
+  for (let i = 0; i < repeatTimes; i++) {
+    output += str
+    for (let j = 0; j < additionRepeatTimes; j++) output += j === additionRepeatTimes - 1 ? addition : addition + additionSeparator
+    output += i === repeatTimes - 1 ? '' : separator
+  }
+  return output
 };
-  
