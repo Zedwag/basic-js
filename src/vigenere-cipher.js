d7f2c3a7127e6fdb91b382f2fda466f5cd033c11
Zedwag <zedwag@gmail.com>
2020-09-18 00:42:20 +0300

    feat: add solution for 10th task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(bool) {
+    if (bool === true || bool === undefined) this.isDirect = true
+    else this.isDirect = false
+  }
+
+  calculateOutput(message, key, encryptOrDescrypt) {
+    if (!message || !key) throw new Error('Not implemented')
+    let output = ''
+    let keyLetterIterator = 0
+    for (let i = 0; i < message.length; i++) {
+      const stringLetter = message[i].toUpperCase()
+      if (stringLetter >= 'A' && stringLetter <= 'Z') {
+        const keyLetter = i < key.length ? key[keyLetterIterator].toUpperCase() : key[keyLetterIterator % key.length].toUpperCase()
+        keyLetterIterator++
+        output += encryptOrDescrypt
+            ? String.fromCharCode((stringLetter.charCodeAt(0) + keyLetter.charCodeAt(0)) % 26 + 65)
+            : String.fromCharCode((26 + stringLetter.charCodeAt(0) - keyLetter.charCodeAt(0)) % 26 + 65)
+      }
+      else output += message[i]
+    }
+    return this.isDirect ? output : output.split('').reverse().join('')
+  }
+
+  encrypt(message, key) {
+    return this.calculateOutput(message, key, true)
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+    return this.calculateOutput(encryptedMessage, key, false)
   }
 }
 
